08daaf806ffdc2a6bb5f7015323286fc2c6e9a5e
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2026-05-07 13:03:53 -0600

    Add zod schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,16 @@
-import { defineCollection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const notes = defineCollection({
-  loader: glob({ pattern: ['*.md', '*.mdx'], base: 'src/notes'}),
+  loader: glob({ pattern: ['*.md', '*.mdx'], base: 'src/notes' }),
+  schema: z.object({
+    title: z.string(),
+    aliases: z.string().optional(),
+    tags: z.array(z.string()).default([]),
+    created: z.coerce.date(),
+    updated: z.coerce.date().optional(),
+    description: z.string().optional(),
+  }),
 });
 
 export const collections = { notes };
---
+++ src/notes/dance-style.md
@@ -0,0 +1,9 @@
+---
+title: Dance Style
+aliases: Dance Style
+tags:
+created: 2026-05-07
+updated: 2026-05-07
+---
+
+See [[the-style-of-no-style.mdx|The Style of No Style]]
